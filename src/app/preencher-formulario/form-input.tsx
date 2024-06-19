4ec65d27d620cadb66c78d5b40cc7e518473957f
gabrielmatiollif@gmail.com <gabrielmatiollif@gmail.com>
2024-06-19 13:14:16 -0300

    Correção erro TS

--- src/app/preencher-formulario/form-input.tsx
+++ src/app/preencher-formulario/form-input.tsx
@@ -11,7 +11,7 @@ interface FormInputProps {
   required: boolean;
   wdth: string;
   list?: string;
-  mask?: string;
+  mask?: "telefone" | "cep";
 }
 
 export const FormInput: React.FC<FormInputProps> = ({
@@ -25,67 +25,49 @@ export const FormInput: React.FC<FormInputProps> = ({
   list,
   mask,
 }) => {
-  let inputElement = (
-    <input
-      type={type}
-      className="form__field"
-      placeholder={label}
-      name={name as string}
-      id={name as string}
-      value={value}
-      onChange={onChange}
-      required={required}
-      list={list}
-    />
-  );
-
-  if (mask === "telefone") {
-    inputElement = (
-      <InputMask
-        mask="+99 (99) 9 9999-9999"
-        value={value}
-        onChange={onChange}
-      >
-        {(inputProps: any) => (
-          <input
-            {...inputProps}
-            type={type}
-            className="form__field"
-            placeholder={label}
-            name={name as string}
-            id={name as string}
-            required={required}
-            list={list}
-          />
-        )}
-      </InputMask>
-    );
-  } else if(mask === "cep"){
-    inputElement = (
-      <InputMask
-        mask="99999-999"
-        value={value}
-        onChange={onChange}
-      >
-        {(inputProps: any) => (
-          <input
-            {...inputProps}
-            type={type}
-            className="form__field"
-            placeholder={label}
-            name={name as string}
-            id={name as string}
-            required={required}
-            list={list}
-          />
-        )}
-      </InputMask>
-    );
-  }
-
   return (
     <div className={`form__group field ${wdth}`}>
-      {inputElement}
+      {mask === "telefone" ? (
+        <InputMask
+          mask="+99 (99) 9 9999-9999"
+          value={value}
+          onChange={onChange}
+        >
+          <input
+            type={type}
+            className="form__field"
+            placeholder={label}
+            name={name as string}
+            id={name as string}
+            required={required}
+            list={list}
+          />
+        </InputMask>
+      ) : mask === "cep" ? (
+        <InputMask mask="99999-999" value={value} onChange={onChange}>
+          <input
+            type={type}
+            className="form__field"
+            placeholder={label}
+            name={name as string}
+            id={name as string}
+            required={required}
+            list={list}
+          />
+        </InputMask>
+      ) : (
+        <input
+          type={type}
+          className="form__field"
+          placeholder={label}
+          name={name as string}
+          id={name as string}
+          value={value}
+          onChange={onChange}
+          required={required}
+          list={list}
+        />
+      )}
       <label htmlFor={name as string} className="form__label">
         {label}
       </label>
