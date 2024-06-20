893d6184576f67f050358c7725c369e4f9a6500d
gabrielmatiollif@gmail.com <gabrielmatiollif@gmail.com>
2024-06-20 15:59:57 -0300

    Criação do modal de pagamento e tem bug na hora de enviar como emergência

--- src/app/preencher-formulario/page.tsx
+++ src/app/preencher-formulario/page.tsx
@@ -4,7 +4,7 @@ import { Spinner } from "@chakra-ui/react";
 
 export default function Home(){
   return(
-    <Suspense fallback={<div className="w-full h-full grid place-items-center"><Spinner size={"lg"}/></div>}>
+    <Suspense fallback={<div className="w-full h-[75vh] grid place-items-center"><Spinner size={"lg"}/></div>}>
       <PreencherFormulario />
     </Suspense>
   )
