505242359a779626cd3c23cdd489583bd2452601
gabrielmatiollif@gmail.com <gabrielmatiollif@gmail.com>
2024-06-21 17:32:35 -0300

    Envio de formulário normal

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    url_base: "https://astech-b7e9hkechkd7a2f4.brazilsouth-01.azurewebsites.net/"
+  }
+};
 
 export default nextConfig;
